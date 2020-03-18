e636aef5552daf6c6170b76a694584fcdb7dde6b
Daniel Martin Ochoa <56157528+dmartinochoa@users.noreply.github.com>
2020-03-18 14:00:14 +0100

    noice button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     private val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e(tag, "onCreate() executed")
+        
+        val button = findViewById<Button>(R.id.button_id)
+        button.setOnClickListener {
+            Log.e(tag, "onCreate() executed")
+        }
     }
 }
