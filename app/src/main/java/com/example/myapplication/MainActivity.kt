5c42b5cc8edbd9b7a40fcd42f9b6b22c62d7edc9
Daniel Martin Ochoa <56157528+dmartinochoa@users.noreply.github.com>
2020-03-18 15:19:50 +0100

    Click count button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -12,10 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val button = findViewById<Button>(R.id.button_id)
-        button.setOnClickListener {
-            Log.e(tag, "onCreate() executed")
+        var countNum = 0
+        var displayMessage = findViewById<TextView>(R.id.displayMessage)
+        displayMessage.text = getString(R.string.count_text, countNum.toString())
+        val countButton = findViewById<Button>(R.id.countButton)
+        countButton.setOnClickListener {
+            countNum++
+            displayMessage.text = getString(R.string.count_text, countNum.toString())
         }
     }
 }
