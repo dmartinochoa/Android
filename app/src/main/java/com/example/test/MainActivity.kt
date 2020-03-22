803c059dd8f7d62038b95a1b0b723583a5d0f2af
Daniel Martin Ochoa <56157528+dmartinochoa@users.noreply.github.com>
2020-03-22 19:19:01 +0100

    cleaner

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.test
 
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
-import kotlinx.android.synthetic.main.activity_profile.*
-
 
 class MainActivity : AppCompatActivity() {
     private val tag = "MainActivity"
@@ -28,7 +24,13 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("city", "Cervera (Spain)");
         intent.putExtra(
             "desc",
-            "Marc Márquez Alentà (Cervera, Lérida, 17 de febrero de 1993) es un piloto de motociclismo español que, actualmente, corre en MotoGP .1​ Ha ganado ocho títulos del Campeonato del Mundo de Motociclismo en tres categorías diferentes:125 cc (2010), Moto2 (2012) y seis veces en MotoGP (2013, 2014, 2016, 2017, 2018 y 2019).2​ Actualmente es piloto del equipo Repsol Honda, 3​ donde ha acumulado 50 victorias y 78 podios en 105 carreras disputadas. En febrero de 2020 extendió su contrato con Honda hasta 2024."
+            "Marc Márquez Alentà (Cervera, Lérida, 17 de febrero de 1993) es un piloto de " +
+                    "motociclismo español que, actualmente, corre en MotoGP .1​ Ha ganado ocho " +
+                    "títulos del Campeonato del Mundo de Motociclismo en tres categorías " +
+                    "diferentes:125 cc (2010), Moto2 (2012) y seis veces en MotoGP (2013, 2014, " +
+                    "2016, 2017, 2018 y 2019).2​ Actualmente es piloto del equipo Repsol Honda, 3​ " +
+                    "donde ha acumulado 50 victorias y 78 podios en 105 carreras disputadas. En " +
+                    "febrero de 2020 extendió su contrato con Honda hasta 2024."
         );
         startActivity(intent)
     }
--- app/src/main/java/com/example/test/ProfileActivity.kt
+++ app/src/main/java/com/example/test/ProfileActivity.kt
@@ -2,7 +2,6 @@ package com.example.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import kotlinx.android.synthetic.main.activity_profile.*
 
 
